f9d2c4dab09f679b87402cfb62a3484726ba1980
LincThai <78945845+LincThai@users.noreply.github.com>
2025-08-24 21:01:21 +1000

    Minor work on fish bash mechanic

--- Assets/Scripts/PlayerFishBash.cs
+++ Assets/Scripts/PlayerFishBash.cs
@@ -5,24 +5,38 @@ public class PlayerFishBash : MonoBehaviour
 {
     // set variables
     // player variables
-    public int playerLives = 3;
+    public int playerMaxLives = 3;
+    public int playerCurrentLives;
     public int playerDamage = 1;
     public int playerAttackCooldown = 5;
     public bool isGuarding = false;
 
     // references
     public GameObject fishBashUi;
-    public GameObject fishAreaTrigger;
+    public GameObject playerHealthBar;
+    public FishingAreaTrigger fishAreaTrigger;
 
     // inputs
     private InputAction guardAction;
     private InputAction AttackActionL;
     private InputAction AttackActionR;
 
+    private void OnEnable()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void PlayerAttack()
+    {
+        // apply damage to enemy
+        // animate the attack
+        // play the sound
     }
 
     public void PlayerTakeDamage(int damageTaken)
@@ -31,10 +45,10 @@ public class PlayerFishBash : MonoBehaviour
         if (!isGuarding)
         {
             // reduce life/health
-            playerLives -= damageTaken;
+            playerCurrentLives -= damageTaken;
 
             // check if player has less than or 0 lives/health
-            if (playerLives <= 0)
+            if (playerCurrentLives <= 0)
             {
                 // kill the player
                 Debug.Log("Player Died");
