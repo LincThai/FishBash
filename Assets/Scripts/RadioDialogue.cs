9eb267c930e04362456df0ca56a47e6a6d1c10b3
LincThai <78945845+LincThai@users.noreply.github.com>
2025-09-21 17:22:25 +1000

    work on dialogue

---
+++ Assets/Scripts/RadioDialogue.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class RadioDialogue : MonoBehaviour
+{
+    // set variables
+
+    // references
+    public TMP_Text dialogueText;
+    public GameObject textBox;
+    public Image icon;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
